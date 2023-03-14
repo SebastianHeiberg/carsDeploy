f93073dde881a7200cbc1373e701430849232ec1
Sebastian Heiberg <seba3673@stud.kea.dk>
2023-03-14 20:55:48 +0100

    ny database2

--- settings.js
+++ settings.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://backwork.azurewebsites.net/api"
+export const API_URL = "https://backwork.azurewebsites.net/api"
 
 export const FETCH_NO_API_ERROR = " (Is the API online or did the endpoint exists ?)"
